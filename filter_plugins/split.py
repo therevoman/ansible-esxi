33635b3feb4dfe14dc0a42f0a8b7c472f999ab15
Nate Revo <nrevo@revoweb.com>
2022-06-06 06:05:46 +0000

    Customizations for revoweb

--- filter_plugins/split.py
+++ filter_plugins/split.py
@@ -6,13 +6,13 @@ import re
 def split_string(string, separator=None, maxsplit=-1):
     try:
         return string.split(separator, maxsplit)
-    except Exception, e:
+    except Exception as e:
         raise errors.AnsibleFilterError('split plugin error: %s, provided string: "%s"' % (str(e),str(string)) )
 
 def split_regex(string, separator_pattern='\s+'):
     try:
         return re.split(separator_pattern, string)
-    except Exception, e:
+    except Exception as e:
         raise errors.AnsibleFilterError('split plugin error: %s, provided string: "%s"' % (str(e),str(string)) )
 
 class FilterModule(object):
--- get_vault_pass.esxi.sh
+++ get_vault_pass.esxi.sh
@@ -2,4 +2,12 @@
 # to store (-l is the label, mb will be different later):
 #   security add-generic-password -a all -D "Ansible Vault" -s "ansible_vault" -l "pass-kind" -w 'pass-here!'
 # ansible allows to prompt for data (to stdout) and have different passwords here
-security find-generic-password -s "ansible_vault" -l "esxi" -w
+#security find-generic-password -s "ansible_vault" -l "esxi" -w
+#
+# Changing to keybase 
+# to store value use this command:
+#   keybase kvstore api -m '{"method": "put", "params": {"options": {"namespace": "esxi", "entryKey": "password", "entryValue": "mypassword"}}}'
+#
+
+keybase kvstore api -m '{"method": "get", "params": {"options": {"namespace": "esxi", "entryKey": "password"}}}' | jq -r '.result.entryValue'
+
